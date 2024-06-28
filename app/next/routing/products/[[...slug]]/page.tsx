f36b4dad97d7eaa37825465fe2c3c70456ac46e7
fahim041 <fahimalsafi@gmail.com>
2024-06-28 16:54:13 -0600

    next features added in app ds & bug fixes

--- app/next/routing/products/[[...slug]]/page.tsx
+++ app/next/routing/products/[[...slug]]/page.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export default function ProductPage({ params: { slug }, searchParams: {sort} }: Props) {
   return <>
-  <div>Product Page {slug}</div>
+  <div>Product Page: {slug.map(e => <span key={e}>{e}{', '}</span>)}</div>
   <div>Sort order: {sort}</div>
   </> ;
 }
--- app/struct.ts
+++ app/struct.ts
@@ -18,16 +18,40 @@ const frameworks: Framework[] = [
     features: [
       {
         id: 1,
-        name: 'Server Side data fetching',
+        name: 'Server side data fetching',
         url: '/next/data-fetching/server/users',
         code: 'https://github.com/fahim041/library/tree/main/app/next/data-fetching/server',
       },
       {
         id: 2,
-        name: 'Client Side data fetching',
+        name: 'Client side data fetching',
         url: '/next/data-fetching/client/users',
         code: 'https://github.com/fahim041/library/tree/main/app/next/data-fetching/client',
       },
+      {
+        id: 3,
+        name: 'Routing - params',
+        url: '/next/routing/users',
+        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/users'
+      },
+      {
+        id: 4,
+        name: 'Routing - multiple params & search params',
+        url: '/next/routing/products/tech/apple/iphone?sort=price',
+        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/products/%5B%5B...slug%5D%5D'
+      },
+      {
+        id: 5,
+        name: 'Api (zod payload validation)',
+        url: '/next/api/users',
+        code: 'https://github.com/fahim041/library/tree/main/app/next/api/users'
+      },
+      {
+        id: 6,
+        name: 'SEO',
+        url: '/next/seo',
+        code: 'https://github.com/fahim041/library/tree/main/app/next/seo'
+      }
     ],
   },
   {
