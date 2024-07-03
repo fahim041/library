f8ce096f10583e78315689760dc55113c85b848f
fahim041 <fahimalsafi@gmail.com>
2024-07-02 21:16:17 -0600

    expense form with react-hook-form & zod

--- app/project/expense-tracker/form.tsx
+++ app/project/expense-tracker/form.tsx
@@ -1,16 +1,46 @@
-import { Category } from './interfaces';
+import { Category, Expense } from './interfaces';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface Props {
   categories: Category[];
 }
 
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: 'Description should be at least 3 characters' }),
+  amount: z.number({ invalid_type_error: 'Amount is required' }),
+  category: z.string().min(3, { message: 'Category is required' }),
+});
+
 export default function Form({ categories }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Expense>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleForm = (data: Expense) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <form className="p-2 w-1/2">
+      <form className="p-2 w-1/2" onSubmit={handleSubmit(handleForm)}>
         <div className="mb-2">
           <label htmlFor="description">Description</label>
-          <input id="description" className="border p-1 w-full mt-1 rounded" />
+          <input
+            id="description"
+            className="border p-1 w-full mt-1 rounded"
+            {...register('description')}
+          />
+          {errors.description && (
+            <p className="text-red-500">{errors.description.message}</p>
+          )}
         </div>
         <div className="mb-2">
           <label htmlFor="amount">Amount</label>
@@ -18,18 +48,33 @@ export default function Form({ categories }: Props) {
             id="amount"
             type="number"
             className="border p-1 w-full mt-1 rounded"
+            {...register('amount')}
           />
+          {errors.amount && (
+            <p className="text-red-500">{errors.amount.message}</p>
+          )}
         </div>
-        <div className='mb-2'>
+        <div className="mb-2">
           <label htmlFor="category">Category</label>
-          <select id="category" className="border p-1 w-full rounded mt-2">
-            <option></option>
+          <select
+            id="category"
+            className="border p-1 w-full rounded mt-2"
+            {...register('category')}
+          >
+            <option value=""></option>
             {categories.map((category) => (
-              <option key={category.id}>{category.name}</option>
+              <option key={category.id} value={category.value}>
+                {category.name}
+              </option>
             ))}
           </select>
+          {errors.category && (
+            <p className="text-red-500">{errors.category.message}</p>
+          )}
         </div>
-        <button className='border px-2 py-1 rounded disabled:bg-white disabled:text-gray-400'>Submit</button>
+        <button className="border px-2 py-1 rounded disabled:bg-white disabled:text-gray-400">
+          Submit
+        </button>
       </form>
     </div>
   );
