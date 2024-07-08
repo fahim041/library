70e2dc98bd75336d7c37afa76365db195e42428b
fahim041 <fahimalsafi@gmail.com>
2024-07-08 16:23:54 -0600

    react features added to homepage

--- app/struct.ts
+++ app/struct.ts
@@ -32,26 +32,26 @@ const frameworks: Framework[] = [
         id: 3,
         name: 'Routing - params',
         url: '/next/routing/users',
-        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/users'
+        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/users',
       },
       {
         id: 4,
         name: 'Routing - multiple params & search params',
         url: '/next/routing/products/tech/apple/iphone?sort=price',
-        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/products/%5B%5B...slug%5D%5D'
+        code: 'https://github.com/fahim041/library/tree/main/app/next/routing/products/%5B%5B...slug%5D%5D',
       },
       {
         id: 5,
         name: 'Api (zod payload validation)',
         url: '/next/api/users',
-        code: 'https://github.com/fahim041/library/tree/main/app/next/api/users'
+        code: 'https://github.com/fahim041/library/tree/main/app/next/api/users',
       },
       {
         id: 6,
         name: 'SEO',
         url: '/next/seo',
-        code: 'https://github.com/fahim041/library/tree/main/app/next/seo'
-      }
+        code: 'https://github.com/fahim041/library/tree/main/app/next/seo',
+      },
     ],
   },
   {
@@ -64,6 +64,30 @@ const frameworks: Framework[] = [
         url: '/react/routing',
         code: 'https://github.com/fahim041/library/tree/main/app/react/routing',
       },
+      {
+        id: 2,
+        name: 'Hook form (zod validation)',
+        url: '/react/hook-form',
+        code: 'https://github.com/fahim041/library/tree/main/app/react/hook-form',
+      },
+      {
+        id: 3,
+        name: 'Reusable fetch hook',
+        url: '/react/fetch-hook',
+        code: 'https://github.com/fahim041/library/tree/main/app/react/fetch-hook'
+      },
+      {
+        id: 4,
+        name: 'Reusable http service',
+        url: '/react/http-service',
+        code: 'https://github.com/fahim041/library/tree/main/app/react/http-service'
+      },
+      {
+        id: 5,
+        name: 'React Query',
+        url: '/react/react-query',
+        code: 'https://github.com/fahim041/library/tree/main/app/react/react-query'
+      }
     ],
   },
 ];
